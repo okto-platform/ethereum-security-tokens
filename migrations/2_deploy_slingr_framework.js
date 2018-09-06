37678d93e480d0c75b9970d61967003b06889c3c
Diego Gaviola <dgaviola@slingr.io>
2018-09-06 12:12:18 -0300

    adding multisig wallet from gnosis

--- migrations/2_deploy_slingr_framework.js
+++ migrations/2_deploy_slingr_framework.js
@@ -3,6 +3,7 @@ var KycModuleFactory = artifacts.require("KycModuleFactory");
 var ExternalTokenOfferingFactory = artifacts.require("ExternalTokenOfferingFactory");
 var TokensHardCapModuleFactory = artifacts.require("TokensHardCapModuleFactory");
 var StandardWhitelistFactory = artifacts.require("StandardWhitelistFactory");
+var MultiSigWalletFactory = artifacts.require("MultiSigWalletFactory");
 
 module.exports = function(deployer) {
   deployer.deploy(SlingrSecurityTokenFactory);
@@ -10,4 +11,5 @@ module.exports = function(deployer) {
   deployer.deploy(ExternalTokenOfferingFactory);
   deployer.deploy(TokensHardCapModuleFactory);
   deployer.deploy(StandardWhitelistFactory);
+  deployer.deploy(MultiSigWalletFactory);
 };
