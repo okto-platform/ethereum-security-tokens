0cd020ed279816fee3fe3a4fde3efa6dd8260604
Diego Gaviola <dgaviola@slingr.io>
2018-10-10 18:53:54 -0300

    defining permissions for security token

--- contracts/tokens/SecurityToken.sol
+++ contracts/tokens/SecurityToken.sol
@@ -3,21 +3,23 @@ pragma solidity ^0.4.24;
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "../utils/Factory.sol";
+import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
 
-contract ISecurityToken is Ownable {
+contract ISecurityToken is Pausable {
     // ERC-20
 
-    //function totalSupply() public view returns (uint256);
+    uint256 public totalSupply;
+
     function balanceOf(address _who) public view returns (uint256);
 
     event Transfer(address indexed from, address indexed to, uint256 value);
 
     // ERC-777
 
-    //function name() public view returns (string);
-    //function symbol() public view returns (string);
-    //function decimals() public view returns (uint8);
-    //function defaultOperators() public view returns (address[]);
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
     function authorizeOperator(address operator) public;
     function revokeOperator(address operator) public;
     function isOperatorFor(address operator, address tokenHolder) public view returns (bool);
@@ -31,19 +33,16 @@ contract ISecurityToken is Ownable {
     function getDestinationTranche(bytes32 sourceTranche, address from, uint256 amount, bytes data) public view returns(bytes32);
     function balanceOfByTranche(bytes32 tranche, address tokenHolder) public view returns (uint256);
     function sendByTranche(bytes32 tranche, address to, uint256 amount, bytes data) public returns (bytes32);
-    //function sendByTranches(bytes32[] tranches, address[] tos, uint256[] amounts, bytes data) public returns (bytes32[]);
     function operatorSendByTranche(bytes32 tranche, address from, address to, uint256 amount, bytes data, bytes operatorData) public returns (bytes32);
-    //function operatorSendByTranches(bytes32[] tranches, address[] froms, address[] tos, uint256[] amounts, bytes data, bytes operatorData) public returns (bytes32[]);
     function tranchesOf(address tokenHolder) public view returns (bytes32[]);
     function redeemByTranche(bytes32 tranche, uint256 amount, bytes data) public;
     function operatorRedeemByTranche(bytes32 tranche, address tokenHolder, uint256 amount, bytes data, bytes operatorData) public;
 
     event SentByTranche(bytes32 fromTranche, bytes32 toTranche, address indexed operator, address indexed from, address indexed to, uint256 amount, bytes data, bytes operatorData);
-    event BurnedByTranche(bytes32 tranche, address indexed operator, address indexed from, uint256 amount, bytes data, bytes operatorData);
+    event RedeemedByTranche(bytes32 tranche, address indexed operator, address indexed from, uint256 amount, bytes data, bytes operatorData);
 
     // ERC-1411
 
-    //function issuable() public view returns (bool);
     function canSend(address _from, address _to, bytes32 _tranche, uint256 _amount, bytes _data) public view returns (byte, bytes32, bytes32);
     function issueByTranche(bytes32 _tranche, address _tokenHolder, uint256 _amount, bytes _data) public;
 
@@ -53,18 +52,13 @@ contract ISecurityToken is Ownable {
 contract SecurityToken is ISecurityToken {
     using SafeMath for uint256;
 
-    enum TokenStatus {Draft, Released}
+    enum TokenStatus {Draft, Ready, InitialOffering, InitialOfferingCompleted, Trading}
 
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
     mapping(address => mapping(bytes32 => uint256)) internal balancesPerTranche;
     mapping(address => uint256) internal balances;
     mapping(address => bytes32[]) internal tranches;
     address[] public defaultOperators;
     mapping(address => mapping(address => bool)) internal operators;
-    bool public issuable;
     TokenStatus public status;
 
 
@@ -74,9 +68,21 @@ contract SecurityToken is ISecurityToken {
     //
     ///////////////////////////////////////////////////////////////////////////
 
-    modifier isReleased()
+    modifier inInitialOffering()
     {
-        require(status == TokenStatus.Released, "Token must be released");
+        require(status == TokenStatus.InitialOffering, "Token is not during initial offering");
+        _;
+    }
+
+    modifier trading()
+    {
+        require(status != TokenStatus.Trading, "Token is not trading");
+        _;
+    }
+
+    modifier notDraft()
+    {
+        require(status != TokenStatus.Draft, "Token is still in draft");
         _;
     }
 
@@ -142,6 +148,8 @@ contract SecurityToken is ISecurityToken {
         return false;
     }
 
+    // TODO allow to change default operators by owner
+
     ///////////////////////////////////////////////////////////////////////////
     //
     // ERC-1410 Partially Fungible Token
@@ -163,7 +171,7 @@ contract SecurityToken is ISecurityToken {
     }
 
     function sendByTranche(bytes32 tranche, address to, uint256 amount, bytes data)
-    isReleased
+    trading whenNotPaused
     public returns (bytes32)
     {
         require(amount <= balancesPerTranche[msg.sender][tranche], "Insufficient funds in tranche");
@@ -174,7 +182,7 @@ contract SecurityToken is ISecurityToken {
     }
 
     function operatorSendByTranche(bytes32 tranche, address from, address to, uint256 amount, bytes data, bytes operatorData)
-    isReleased
+    notDraft whenNotPaused
     public returns (bytes32)
     {
         require(isOperatorFor(msg.sender, from), "Invalid operator");
@@ -189,8 +197,6 @@ contract SecurityToken is ISecurityToken {
     function internalSendByTranche(bytes32 tranche, address operator, address from, address to, uint256 amount, bytes data, bytes operatorData)
     internal returns (bytes32)
     {
-        // TODO call tokensToSend if n  eeded
-        // TODO call tokensReceived if needed
         // TODO call canSend
 
         bytes32 destinationTranche = getDestinationTranche(tranche, to, amount, data);
@@ -214,7 +220,7 @@ contract SecurityToken is ISecurityToken {
     }
 
     function redeemByTranche(bytes32 tranche, uint256 amount, bytes data)
-    isReleased
+    notDraft whenNotPaused
     public
     {
         require(amount <= balancesPerTranche[msg.sender][tranche], "Insufficient funds in tranche");
@@ -223,7 +229,7 @@ contract SecurityToken is ISecurityToken {
     }
 
     function operatorRedeemByTranche(bytes32 tranche, address tokenHolder, uint256 amount, bytes data, bytes operatorData)
-    isReleased
+    notDraft
     public
     {
         require(amount <= balancesPerTranche[msg.sender][tranche], "Insufficient funds in tranche");
@@ -236,7 +242,7 @@ contract SecurityToken is ISecurityToken {
     function internalRedeemByTranche(bytes32 tranche, address operator, address tokenHolder, uint256 amount, bytes data, bytes operatorData)
     internal
     {
-        // TODO call tokensToSend if needed
+        // TODO call canSend with the destination address 0
         balancesPerTranche[tokenHolder][tranche] = balancesPerTranche[tokenHolder][tranche].sub(amount);
         // TODO remove tranche if the balance is zero for the source
         // update global balances
@@ -244,7 +250,7 @@ contract SecurityToken is ISecurityToken {
         // reduce total supply of tokens
         totalSupply = totalSupply.sub(amount);
         // trigger events
-        emit BurnedByTranche(tranche, operator, tokenHolder, amount, data, operatorData);
+        emit RedeemByTranche(tranche, operator, tokenHolder, amount, data, operatorData);
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -262,7 +268,7 @@ contract SecurityToken is ISecurityToken {
     }
 
     function issueByTranche(bytes32 tranche, address tokenHolder, uint256 amount, bytes data)
-    isReleased
+    inOffering onlyOwner
     public
     {
         require(tokenHolder != address(0), "Cannot issue tokens to address 0x0");
@@ -300,13 +306,40 @@ contract SecurityToken is ISecurityToken {
         return status;
     }
 
-    function release()
+    function ready()
+    onlyOwner
     public
     {
-        require(status == TokenStatus.Draft, "Token is not in draft status");
-        require(isDefaultOperator(msg.sender), "Only default operators can do this");
+        require(status != TokenStatus.Draft, "Token must be in draft");
 
-        status = TokenStatus.Released;
+        status = TokenStatus.Ready;
+    }
+
+    function startInitialOffering()
+    onlyOwner
+    public
+    {
+        require(status != TokenStatus.Ready, "Token must be ready");
+
+        status = TokenStatus.InitialOffering;
+    }
+
+    function endInitialOffering()
+    onlyOwner
+    public
+    {
+        require(status != TokenStatus.InitialOffering, "Token must be in initial offering");
+
+        status = TokenStatus.InitialOfferingCompleted;
+    }
+
+    function startTrading()
+    onlyOwner
+    public
+    {
+        require(status != TokenStatus.InitialOfferingCompleted, "Initial offering must be completed");
+
+        status = TokenStatus.Trading;
     }
 }
 
--- test/tokens/TestSecurityToken.js
+++ test/tokens/TestSecurityToken.js
@@ -12,11 +12,13 @@ let checkEvent = function(result, eventName) {
 };
 
 contract('SecurityTokenFactory', async(accounts) => {
+    let tokenAddress;
+
     it('configure simple token', async() => {
         let factory = await SecurityTokenFactory.deployed();
         await factory.createInstance('Token A', 'TOKA', 18, [accounts[0]], {from: accounts[0]});
         let tokensCount = await factory.getInstancesCount.call();
-        let tokenAddress = await factory.getInstance.call(tokensCount - 1);
+        tokenAddress = await factory.getInstance.call(tokensCount - 1);
         let token = await SecurityToken.at(tokenAddress);
 
         let name = await token.name.call();
@@ -31,5 +33,20 @@ contract('SecurityTokenFactory', async(accounts) => {
         assert.equal(totalSupply, 0, 'Token total supply is not set to zero at creation time');
         assert.equal(issuable, true, 'Token is not issuable when it is created');
         assert.equal(status, 0, 'Token is not in draft status after created');
+    });
+
+    it('configure token', async() => {
+        let token = await SecurityToken.at(tokenAddress);
+        await token.release({from: accounts[0]});
+
+        let status = await token.status.call();
+        assert.equals(status, 1, 'Token is not released');
+    });
+
+    it('issue tokens', async() => {
+    });
+
+
+    it('transfer tokens', async() => {
     });
 });
